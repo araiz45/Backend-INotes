795bcf6a4786c716a188caaa76e4b06cac56dfd0
araiz45 <araizzahid45@gmail.com>
2023-08-07 23:42:33 +0500

    video 45 completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require("express")
+const user = require("../models/user");
+
 const router = express.Router();
 
-router.get("/", (req, res) =>{
-    let obj = {
-        name: "injir",
-        phone: "343434334"
-    }
-    res.json(obj)
+router.post("/", (req, res) =>{
+   console.log(req.body);
+   const responseUser = user(req.body);
+   responseUser.save();
+   res.send("send");
 })
 
 
--- routes/notes.js
+++ routes/notes.js
@@ -2,11 +2,8 @@ const express = require("express")
 const router = express.Router();
 
 router.get("/", (req, res) =>{
-    let obj = {
-        name: "injir",
-        phone: "343434334"
-    }
-    res.json(obj)
+    console.log(req.body)
+   res.send("hello")
 })
 
 
